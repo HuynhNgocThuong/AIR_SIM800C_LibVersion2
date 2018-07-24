85d141fc2e4dea1d1f248fc0a7f9ef162ec9b5cd
HuynhNgocThuong <huynhngocthuong0208@gmail.com>
2018-07-24 19:17:35 +0700

    changes
    Xoa con tro int *a;

--- Src/main.c
+++ Src/main.c
@@ -19,7 +19,6 @@ UART_HandleTypeDef huart3;
 #define false 0
 #define bool int
 	int i, n=10;
- int *a;
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
